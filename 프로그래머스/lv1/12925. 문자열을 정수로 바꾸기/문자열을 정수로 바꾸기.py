6ed9e5c5f98bebd62a11404b81a87f2d4f8bf50e
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2023-05-18 00:20:21 +0900

    [level 1] Title: 문자열을 정수로 바꾸기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
+++ 프로그래머스/lv1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.py
@@ -1,9 +1,13 @@
 import re
 def solution(s):
-    if re.match(r"^\-", s):
-        return -int(s[1:])
-    else:
-        return int(s)
+    
+    return int(s)
+    
+    # if re.match(r"^\-", s):
+    #     return int(s)
+    #     #return -int(s[1:])
+    # else:
+    #     return int(s)
 
 # if re.match(r"^[0-9]+$",s):
 #     return int(s)
