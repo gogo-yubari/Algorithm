0b923ad3f346cc7c995116f856dbfc08f9106d08
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2023-05-28 19:04:54 +0900

    [level 0] Title: 편지, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.py
@@ -0,0 +1,8 @@
+def solution(message):
+#     count = 0
+#     for char in message:
+#         if char.isalpha():
+#             count += 1
+        
+#     return count*2 + len(message)-count
+    return len(message)*2
