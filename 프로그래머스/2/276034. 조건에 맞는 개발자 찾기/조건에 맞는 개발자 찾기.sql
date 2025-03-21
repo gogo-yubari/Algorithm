a2c4066fdeca88a30f64f2ba7e60beb975f9bfbf
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2025-03-21 12:17:46 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -1,6 +1,9 @@
 -- 코드를 작성해주세요
 select distinct ID,	EMAIL, FIRST_NAME,	LAST_NAME
-from DEVELOPERS
-where SKILL_CODE & 1280 > 0
+from DEVELOPERS d
+join SKILLCODES s
+on d.SKILL_CODE & s.CODE
+where s.NAME in ('Python','C#')
 order by 1
 # 두 기술 모두 보유한 경우 중복출력되기 때문에 중복 제거를 해줘야함
+# python, c#의 code가 256,1024로 고정된 게 아님
