ff71798ddd0778ca0eebaec1bd6226a67508e625
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2025-03-21 21:43:24 +0900

    [level 0] Title: 홀짝 구분하기, Time: 16.45 ms, Memory: 7.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181944. 홀짝 구분하기/홀짝 구분하기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    return f"{n} is {'even' if n%2==0 else 'odd'}"
+
+
+a = int(input())
+print(solution(a))
+
+
+# if a%2==0:
+#     print(f"{a} is even")
+# else:
+#     print(f"{a} is odd")
+
+
+
+
