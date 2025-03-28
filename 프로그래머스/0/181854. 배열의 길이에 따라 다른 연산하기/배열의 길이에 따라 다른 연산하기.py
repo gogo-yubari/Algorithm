1b4cff29579d9679b02251ac1384464bfcebaf29
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2025-03-29 00:07:40 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.37 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.py
@@ -1,8 +1,17 @@
-# def solution(arr, n):
-#     return [ v+n for i,v in enumerate(arr) if not i%2 ] if len(arr)%2 else [ v+n for i,v in enumerate(arr) if i%2 ] 
+
 
 def solution(arr, n):
-    if len(arr) % 2:
-        return [v + n if i % 2 == 0 else v for i, v in enumerate(arr)]  # 홀수 길이일 때 짝수 인덱스에만 n을 더함
-    else:
-        return [v + n if i % 2 != 0 else v for i, v in enumerate(arr)]  # 짝수 길이일 때 홀수 인덱스에만 n을 더함
+    return [v+n if (i%2==0 and len(arr)%2==1) 
+            or (i%2==1 and len(arr)%2==0) 
+            else v for i,v in enumerate(arr)]
+
+# def solution(arr, n):
+#     return [v+n for i,v in enumerate(arr) if not i%2] 
+#             if len(arr)%2 else [v+n for i,v in enumerate(arr) if i%2]     
+# 이건 짝수행,홀수행만 나와버림..    
+
+# def solution(arr, n):
+#     if len(arr) % 2:
+#         return [v + n if i % 2 == 0 else v for i, v in enumerate(arr)]  
+#     else:
+#         return [v + n if i % 2 != 0 else v for i, v in enumerate(arr)] 
