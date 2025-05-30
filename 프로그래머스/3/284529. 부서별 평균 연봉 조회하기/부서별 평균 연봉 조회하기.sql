bf96dbf96bf8506630010dda96e0b4636dc3fa11
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2025-05-31 01:01:04 +0900

    [level 3] Title: 부서별 평균 연봉 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/284529. 부서별 평균 연봉 조회하기/부서별 평균 연봉 조회하기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+select a.DEPT_ID, a.DEPT_NAME_EN, 
+ROUND(AVG(b.SAL)) as AVG_SAL
+from HR_DEPARTMENT a
+join HR_EMPLOYEES b
+on a.DEPT_ID = b.DEPT_ID
+group by a.DEPT_ID
+order by AVG_SAL desc
