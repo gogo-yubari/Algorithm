d39d95fc68870d053f9d144f330ae7cf69d8368a
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2025-05-31 17:46:51 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,17 +1,18 @@
 -- 코드를 입력하세요
-# SELECT a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
-# from APPOINTMENT a
-# join DOCTOR d on a.MDDR_ID = d.DR_ID
-# join PATIENT p on a.PT_NO = p.PT_NO
-# where a.APNT_CNCL_YN = 'N' and a.MCDP_CD = 'CS' and a.APNT_YMD like '2022-04-13%'
-
 SELECT a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
-FROM APPOINTMENT as a
-LEFT JOIN PATIENT as p
-ON a.PT_NO = p.PT_NO
-LEFT JOIN DOCTOR as d
-ON a.MDDR_ID = d.DR_ID
-WHERE a.APNT_YMD LIKE '2022-04-13%'
-AND a.APNT_CNCL_YN = 'N'
-AND a.MCDP_CD = 'CS'
-ORDER BY a.APNT_YMD
+from APPOINTMENT a
+left join DOCTOR d on a.MDDR_ID = d.DR_ID
+left join PATIENT p on a.PT_NO = p.PT_NO
+where a.APNT_CNCL_YN = 'N' and a.MCDP_CD = 'CS' and a.APNT_YMD like '2022-04-13%'
+order by APNT_YMD
+
+# SELECT a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
+# FROM APPOINTMENT as a
+# LEFT JOIN PATIENT as p
+# ON a.PT_NO = p.PT_NO
+# LEFT JOIN DOCTOR as d
+# ON a.MDDR_ID = d.DR_ID
+# WHERE a.APNT_YMD LIKE '2022-04-13%'
+# AND a.APNT_CNCL_YN = 'N'
+# AND a.MCDP_CD = 'CS'
+# ORDER BY a.APNT_YMD
