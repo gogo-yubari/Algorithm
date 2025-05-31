6f5c47dd0900c623b321bea50e2f92c6d841976e
gogo-yubari <124599667+gogo-yubari@users.noreply.github.com>
2025-05-31 17:49:52 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,18 +1,10 @@
 -- 코드를 입력하세요
 SELECT a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
 from APPOINTMENT a
-left join DOCTOR d on a.MDDR_ID = d.DR_ID
-left join PATIENT p on a.PT_NO = p.PT_NO
-where a.APNT_CNCL_YN = 'N' and a.MCDP_CD = 'CS' and a.APNT_YMD like '2022-04-13%'
+join DOCTOR d on a.MDDR_ID = d.DR_ID
+join PATIENT p on a.PT_NO = p.PT_NO
+where a.APNT_CNCL_YN = 'N' and a.MCDP_CD = 'CS' and DATE(a.APNT_YMD) = '2022-04-13%'
 order by APNT_YMD
 
-# SELECT a.APNT_NO, p.PT_NAME, p.PT_NO, a.MCDP_CD, d.DR_NAME, a.APNT_YMD
-# FROM APPOINTMENT as a
-# LEFT JOIN PATIENT as p
-# ON a.PT_NO = p.PT_NO
-# LEFT JOIN DOCTOR as d
-# ON a.MDDR_ID = d.DR_ID
-# WHERE a.APNT_YMD LIKE '2022-04-13%'
-# AND a.APNT_CNCL_YN = 'N'
-# AND a.MCDP_CD = 'CS'
-# ORDER BY a.APNT_YMD
+# 문제 오류있네 열받아
+# 출력 43 바라 pt22000019 밖에 출력 안되는데 미친것
